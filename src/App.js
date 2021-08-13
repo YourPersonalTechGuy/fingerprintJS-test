8f0b5e46e2dcb8994101eb0ba4836b9c5b87f786
Anas Abdelsalam <anas.abdelsalam57@gmail.com>
2021-08-13 00:32:10 -0500

    adding depedencies and adding fingerprint.js implementation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "antd";
+import FingerprintJS from '@fingerprintjs/fingerprintjs-pro'
 
 function App() {
+
+  FingerprintJS.load({token: 'vRAfqZN2JkpjoXL6tAaM'})
+  .then(fp => fp.get())
+  .then(result => console.log(result.visitorId))
+
   return (
     <div className="App">
       <header className="App-header">
