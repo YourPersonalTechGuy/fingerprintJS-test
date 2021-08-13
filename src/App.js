43569066b79c4044f36daf29b4f4335932bcc69d
Anas Abdelsalam <anas.abdelsalam57@gmail.com>
2021-08-13 03:15:52 -0500

    implemented table and API calls

--- src/App.js
+++ src/App.js
@@ -1,48 +1,138 @@
 import "./App.css";
 import { Table } from "antd";
 import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
-// import axios from "axios";
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { DateTime } from "luxon";
 
 function App() {
   let [visitorID, setVisitorID] = useState(null);
   let [fpInfo, setFpInfo] = useState(null);
 
-  FingerprintJS.load({ token: "vRAfqZN2JkpjoXL6tAaM" })
-    .then((fp) => fp.get())
-    .then((result) => {
-      console.log(result);
-      setVisitorID(result.visitorId);
-      setFpInfo(null);
-      console.log(visitorID);
-    });
+  useEffect(() => {
+    FingerprintJS.load({ token: "KOvSOQ8tGOvM1XhMY7YG" })
+      .then((fp) => fp.get())
+      .then((result) => {
+        console.log(result);
+        setVisitorID(result.visitorId);
+      });
+  });
+
+  useEffect(() => {
+    console.log(visitorID);
+    axios
+      .get(
+        `https://api.fpjs.io/visitors/${visitorID}?token=EoaDEifytrYzG7tca3df`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setFpInfo(res.data.visits);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [visitorID]);
+
+  useEffect(() => {
+    console.log(fpInfo);
+  }, [fpInfo]);
 
   const columns = [
     {
-      title: "Description",
-      dataIndex: "desc",
-      key: "desc",
-      ellipsis: true,
+      title: "RequestID",
+      dataIndex: "requestId",
       fixed: "top",
     },
     {
-      title: "City",
-      dataIndex: "city",
-      key: "city",
-      align: "left",
+      title: "IP",
+      dataIndex: "ip",
       fixed: "top",
     },
     {
-      title: "State",
-      dataIndex: "state",
-      key: "state",
+      title: "Device",
+      dataIndex: "browserDetails",
+      fixed: "top",
+      render: (data) => {
+        return `${data.device}`;
+      },
+    },
+    {
+      title: "Browser",
+      dataIndex: "browserDetails",
+      fixed: "top",
+      render: (data) => {
+        return `${data.browserName} ${data.browserFullVersion}`;
+      },
+    },
+    {
+      title: "Operating System",
+      dataIndex: "browserDetails",
+      fixed: "top",
+      render: (data) => {
+        return `${data.os} ${data.osVersion}`;
+      },
+    },
+    {
+      title: "Incognito",
+      dataIndex: "incognito",
+      fixed: "top",
+      render: (data) => {
+        return data ? "Yes" : "No";
+      },
+    },
+    {
+      title: "Location",
+      dataIndex: "ipLocation",
+      fixed: "top",
+      render: (data) => {
+        return `${data.city.name} ${data.subdivisions[0].isoCode}, ${data.postalCode}, ${data.country.name}`;
+      },
+    },
+    {
+      title: "Date and Time",
+      dataIndex: "time",
+      key: "time",
+      fixed: "top",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => {
+        let aTime = DateTime.fromISO(a.time);
+        let bTime = DateTime.fromISO(b.time);
+
+        return aTime - bTime;
+      },
+      render: (data) => {
+        return `${data.slice(0, 10)} ${data.slice(11, 19)}  `;
+      },
+    },
+    {
+      title: "Time Zone",
+      dataIndex: "ipLocation",
+      fixed: "top",
+      render: (data) => {
+        return `${data.timezone}`;
+      },
+    },
+    {
+      title: "IP",
+      dataIndex: "ip",
       fixed: "top",
     },
   ];
 
   return (
     <div className="App">
-      <Table columns={columns} dataSource={fpInfo} />
+      <h1>Your ID is: {visitorID}</h1>
+      <Table 
+      columns={columns}
+      dataSource={fpInfo} 
+      key={"requestId"} 
+      pagination={{
+        position: ['bottomCenter'],
+        total: fpInfo ? fpInfo.length : 0,
+        showTotal(total, range) {
+          return `${range[0]}-${range[1]} of ${total} items`;
+        },
+      }} />
     </div>
   );
 }
